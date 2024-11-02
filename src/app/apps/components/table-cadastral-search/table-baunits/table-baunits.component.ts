12637d0f28f8f972d4f7370c9c416fa9f2d821df
david.aristizabal <david.aristizabal@kiplan.io>
2024-11-02 13:16:31 -0500

    Update 02/11/2024

---
+++ src/app/apps/components/table-cadastral-search/table-baunits/table-baunits.component.ts
@@ -0,0 +1,360 @@
+import { AfterViewInit, Component, DestroyRef, inject, Input, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { BaunitHead } from 'src/app/apps/interfaces/information-property/baunit-head.model';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { TableColumn } from '@vex/interfaces/table-column.interface';
+import { SelectionModel } from '@angular/cdk/collections';
+import { fadeInUp400ms } from '@vex/animations/fade-in-up.animation';
+import { stagger40ms } from '@vex/animations/stagger.animation';
+import { FormsModule, ReactiveFormsModule, UntypedFormControl } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
+import { AsyncPipe, NgClass, NgFor, NgIf } from '@angular/common';
+import { VexPageLayoutContentDirective } from '@vex/components/vex-page-layout/vex-page-layout-content.directive';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { VexBreadcrumbsComponent } from '@vex/components/vex-breadcrumbs/vex-breadcrumbs.component';
+import { VexPageLayoutHeaderDirective } from '@vex/components/vex-page-layout/vex-page-layout-header.directive';
+import { VexPageLayoutComponent } from '@vex/components/vex-page-layout/vex-page-layout.component';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { MatInputModule } from '@angular/material/input';
+import { InfoTableService } from 'src/app/apps/services/general/info-table.service';
+import { SearchData } from 'src/app/apps/interfaces/search-data.model';
+import { InformationPegeable } from 'src/app/apps/interfaces/information-pegeable.model';
+import { PageSearchData } from 'src/app/apps/interfaces/page-search-data.model';
+import { Observable } from 'rxjs';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatSelectModule } from '@angular/material/select';
+import { VexSecondaryToolbarComponent } from '@vex/components/vex-secondary-toolbar/vex-secondary-toolbar.component';
+import { VexLayoutService } from '@vex/services/vex-layout.service';
+import {
+  LIST_SCHEMAS_CONTROL_MAIN,
+  PAGE,
+  PAGE_SIZE_OPTION,
+  PAGE_SIZE_TABLE_CADASTRAL,
+  TABLE_COLUMN_PROPERTIES,
+} from 'src/app/apps/constants/constant';
+import { ContentInfoSchema } from 'src/app/apps/interfaces/content-info-schema';
+import { GeographicViewerComponent } from '../../geographic-viewer/geographic-viewer.component';
+import { environment as envi } from 'src/environments/environments';
+import { SendInformationRegisterService } from 'src/app/apps/services/register-procedure/send-information-register.service';
+import { FluidHeightDirective } from 'src/app/apps/directives/fluid-height.directive';
+import { ValidateInformationBaunitService } from 'src/app/apps/services/general/validate-information-baunit.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { LayoutCardCadastralInformationPropertyComponentComponent } from '../../information-property/layout-card-cadastral-information-property-component/layout-card-cadastral-information-property-component.component';
+
+
+@Component({
+  selector: 'vex-table-cadastral-search',
+  templateUrl: './table-baunits.component.html',
+  styleUrls: ['./table-baunits.component.scss'],
+  animations: [fadeInUp400ms, stagger40ms],
+  standalone: true,
+  imports: [
+    VexPageLayoutComponent,
+    VexPageLayoutHeaderDirective,
+    VexBreadcrumbsComponent,
+    MatButtonToggleModule,
+    ReactiveFormsModule,
+    VexPageLayoutContentDirective,
+    NgIf,
+    MatButtonModule,
+    MatTooltipModule,
+    MatIconModule,
+    MatMenuModule,
+    MatTableModule,
+    MatSortModule,
+    MatCheckboxModule,
+    NgFor,
+    NgClass,
+    MatPaginatorModule,
+    FormsModule,
+    MatDialogModule,
+    MatInputModule,
+    MatTabsModule,
+    MatSelectModule,
+    AsyncPipe,
+    VexSecondaryToolbarComponent,
+    FluidHeightDirective
+  ]
+})
+export class TableCadastralSearchComponent implements OnInit, AfterViewInit {
+
+  isDesktop$: Observable<boolean> = this.layoutService.isDesktop$;
+  contentInformation!: InformationPegeable;
+  searchData!: SearchData;
+
+  @Input()
+  columns: TableColumn<BaunitHead>[] = TABLE_COLUMN_PROPERTIES;
+  page = PAGE;
+  totalElements: number = 0;
+  pageSize: number = PAGE_SIZE_TABLE_CADASTRAL;
+  pageSizeOptions: number[] = PAGE_SIZE_OPTION;
+
+  dataSource!: MatTableDataSource<BaunitHead>;
+  selection: SelectionModel<BaunitHead> = new SelectionModel<BaunitHead>(true, []);
+  searchCtrl: UntypedFormControl = new UntypedFormControl();
+
+  @ViewChild(MatPaginator, { read: true }) paginator?: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort?: MatSort;
+
+  private readonly destroyRef: DestroyRef = inject(DestroyRef);
+
+  constructor(
+    private router: Router,
+    private dialog: MatDialog,
+    private snackbar: MatSnackBar,
+    private infoTableService: InfoTableService,
+    private readonly layoutService: VexLayoutService,
+    private sendInformation: SendInformationRegisterService,
+    private baunitService: ValidateInformationBaunitService
+  ) {
+  }
+
+  get visibleColumns() {
+    return this.columns
+      .filter((column) => column.visible)
+      .map((column) => column.property);
+  }
+
+  ngOnInit(): void {
+    this.dataSource = new MatTableDataSource();
+    this.searchCtrl.valueChanges
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((value) => this.onFilterChange(value));
+  }
+
+  ngAfterViewInit(): void {
+    if (this.paginator) {
+      this.dataSource.paginator = this.paginator;
+    }
+
+    if (this.sort) {
+      this.dataSource.sort = this.sort;
+    }
+  }
+
+  openGeographicViewerMain(data: BaunitHead): void {
+    this.dialog
+      .open(GeographicViewerComponent, {
+        width: '30%',
+        height: '30%',
+        disableClose: true,
+        data: new ContentInfoSchema(data.baunitIdE, data)
+      })
+      .afterClosed();
+  }
+
+  openCadastralInformationProperty(data: BaunitHead): void {
+    this.dialog
+      .open(LayoutCardCadastralInformationPropertyComponentComponent, {
+        minWidth: '99%',
+        minHeight: '90%',
+        disableClose: true,
+        data: new ContentInfoSchema(
+          data.baunitIdE, data, null,
+          LIST_SCHEMAS_CONTROL_MAIN
+        )
+      })
+      .afterClosed();
+  }
+
+  captureInformationCadastralData(): void {
+    let data: BaunitHead[];
+    if (this.contentInformation?.content != null) {
+      data = this.contentInformation.content;
+      data = data.map((row: BaunitHead) => new BaunitHead(row));
+      this.dataSource.data = data;
+    }
+
+    if (this.contentInformation == null) {
+      this.page = PAGE;
+      return;
+    }
+
+    if (this.contentInformation.totalElements) {
+      this.totalElements = this.contentInformation.totalElements;
+    }
+
+    if (this.contentInformation.pageable == null) {
+      this.page = PAGE;
+      return;
+    }
+
+    if (this.contentInformation.pageable.pageNumber != null) {
+      this.page = this.contentInformation.pageable.pageNumber;
+    }
+  }
+
+  searchPropertiesByRegistration(data: SearchData): void {
+    this.infoTableService.getDataPropertyByRegistration(this.generateObjectPageSearchData(data))
+      .subscribe(
+        {
+          error: () => this.captureInformationSubscribeError(),
+          next: (result: InformationPegeable) => this.captureInformationSubscribe(result)
+        }
+      );
+  }
+
+  searchPropertiesByDocument(data: SearchData): void {
+    this.infoTableService.getDataPropertyByDocument(this.generateObjectPageSearchData(data))
+      .subscribe(
+        {
+          error: () => this.captureInformationSubscribeError(),
+          next: (result: InformationPegeable) => this.captureInformationSubscribe(result)
+        }
+      );
+  }
+
+  searchPropertiesByName(data: SearchData): void {
+    this.infoTableService.getDataPropertyByName(this.generateObjectPageSearchData(data))
+      .subscribe(
+        {
+          error: () => this.captureInformationSubscribeError(),
+          next: (result: InformationPegeable) => this.captureInformationSubscribe(result)
+        }
+      );
+  }
+
+  searchPropertiesByAddress(data: SearchData): void {
+    this.infoTableService.getDataPropertyByAddress(this.generateObjectPageSearchData(data))
+      .subscribe({
+        error: () => this.captureInformationSubscribeError(),
+        next: (result: InformationPegeable) => this.captureInformationSubscribe(result)
+      });
+  }
+
+  searchNationalPredialNumber(data: SearchData): void {
+    this.infoTableService.getDataNationalPredialNumber(this.generateObjectPageSearchData(data))
+      .subscribe({
+        error: () => this.captureInformationSubscribeError(),
+        next: (result: InformationPegeable) => this.captureInformationSubscribe(result)
+      });
+  }
+
+  validateRefreshCadastralData(): boolean {
+    if (this.searchData == null) {
+      return false;
+    }
+    let searchData: SearchData = this.searchData;
+
+    if (this.isValidateField(searchData?.registration)) {
+      this.searchPropertiesByRegistration(this.searchData);
+      return true;
+    }
+
+    if (this.isValidateField(searchData?.number) &&
+      this.isValidateField(searchData?.domIndividualTypeNumber)) {
+      this.searchPropertiesByDocument(this.searchData);
+      return true;
+    }
+
+    if (this.isValidateField(searchData?.firstName) &&
+      this.isValidateField(searchData?.lastName)) {
+      this.searchPropertiesByName(this.searchData);
+      return true;
+    }
+
+    if (this.isValidateField(searchData?.textAddress)) {
+      this.searchPropertiesByAddress(this.searchData);
+      return true;
+    }
+
+    if (searchData.sidewalk !== null && searchData.sidewalk !== undefined && searchData.sidewalk.length > 10 ||
+      searchData.block !== null && searchData.block !== undefined && searchData.block.length > 10) {
+      this.searchNationalPredialNumber(this.searchData);
+      return true;
+    }
+    return false;
+  }
+
+  deleteInformations(customer: BaunitHead): void {
+  }
+
+  onFilterChange(value: string): void {
+    if (!this.dataSource) {
+      return;
+    }
+    value = value.trim();
+    value = value.toLowerCase();
+    this.dataSource.filter = value;
+  }
+
+  toggleColumnVisibility(column: TableColumn<BaunitHead>, event: Event) {
+    event.stopPropagation();
+    event.stopImmediatePropagation();
+    column.visible = !column.visible;
+  }
+
+  /** Whether the number of selected elements matches the total number of rows. */
+  isAllSelected(): boolean {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle(): void {
+    this.isAllSelected()
+      ? this.selection.clear()
+      : this.dataSource.data.forEach((row) => this.selection.select(row));
+  }
+
+  trackByProperty<T>(index: number, column: TableColumn<T>): string {
+    return column.property;
+  }
+
+  refreshInformationPaginator(event: PageEvent): void {
+    if (event == null) {
+      return;
+    }
+    this.page = event.pageIndex;
+    this.pageSize = event.pageSize;
+
+    const validate: boolean = this.validateRefreshCadastralData();
+    if (!validate) {
+      throw new Error('No fue posible actualizar los datos de la tabla');
+    }
+  }
+
+  captureInformationSubscribe(result: InformationPegeable): void {
+    this.contentInformation = result;
+    this.captureInformationCadastralData();
+  }
+
+  captureInformationSubscribeError(): void {
+    this.contentInformation = new InformationPegeable();
+    this.dataSource.data = [];
+  }
+
+  generateObjectPageSearchData(data: SearchData): PageSearchData {
+    return new PageSearchData(this.page, this.pageSize, data);
+  }
+
+  async initiateFilingProcedure(data: BaunitHead) {
+    if (data && data?.baunitIdE) {
+      const available = await this.baunitService.getBaunitIdEInOtherProcess(data?.baunitIdE);
+      if (!available){
+        this.snackbar.open(
+          'No se puede radicar un nuevo control de cambios, unidad predial ya se encuentra actualmente en otro.',
+          'CLOSE', { duration: 1000 }
+        );
+        return;
+      }
+      let url: string = `${envi.initiate_filing_procedure}`;
+      this.sendInformation.setInformationRegister(data);
+      this.router.navigate([`${url}`, data.baunitIdE])
+        .then(r => {
+        });
+    }
+  }
+
+  isValidateField(value: string | undefined): boolean {
+    return value !== null && value !== undefined && value.length >= 1;
+  }
+}
