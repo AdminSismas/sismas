47bd4e274e41b31b52d1c995b73e063432b24178
david.aristizabal <david.aristizabal@kiplan.io>
2024-11-20 11:57:38 -0500

    Agregado el validador de suma de fracciones tanto al momento de agregar un nuevo propietario como al editarlo

--- src/app/apps/components/information-property/information-property-owners/add-property-owner/add-property-owner.component.ts
+++ src/app/apps/components/information-property/information-property-owners/add-property-owner/add-property-owner.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Inject, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, UntypedFormControl, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, UntypedFormControl, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -8,7 +8,7 @@ import { ComboxColletionComponent } from '../../../combox-colletion/combox-colle
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { ParamsRrright, AddPropertyOwnerData } from 'src/app/apps/interfaces/bpm/changes-property-owner';
+import { ParamsRrright, AddPropertyOwnerData, Owners } from 'src/app/apps/interfaces/bpm/changes-property-owner';
 import { PeopleService } from 'src/app/apps/services/people.service';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core'
@@ -42,18 +42,23 @@ import { MatDividerModule } from '@angular/material/divider';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AddPropertyOwnerComponent implements OnInit {
+  public customer?: InfoPerson;
+  public fractions_sum: number = 0;
 
   public form: FormGroup = this.fb.group({
     individualTypeNumber: ['', Validators.required],
     number: ['', Validators.required],
   })
   public secondForm: FormGroup = this.fb.group({
-    fraction: [0, Validators.required],
+    fraction: [0, [
+      Validators.required,
+      Validators.max(1),
+      Validators.min(0),
+      this.createFractionValidator()
+    ]],
     domRightType: ['', Validators.required],
     beginAt: [Date(), Validators.required],
   })
-
-  public customer?: InfoPerson;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public defaults: AddPropertyOwnerData,
@@ -67,6 +72,11 @@ export class AddPropertyOwnerComponent implements OnInit {
 
   ngOnInit(): void {
     console.log(this.defaults)
+    this.fractions_sum = this.defaults.ownersData.reduce((acc: number, owner: Owners) => {
+      const fraction = Number(owner.fractionS)
+      return acc + fraction ;
+    }, 0)
+
     this.secondForm.disable()
   }
 
@@ -100,6 +110,13 @@ export class AddPropertyOwnerComponent implements OnInit {
       params: { ...this.secondForm.value, beginAt: formatBeginAt, individual: { individualId: this.customer!.individualId } }
     }
 
+    this.fractions_sum += Number(this.secondForm.value.fraction)
+
+    if (this.fractions_sum > 1) {
+      this.snackbar.open('La suma de fracciones es mayor a 1', 'CLOSE', { duration: 4000 })
+      return;
+    }
+
     this.rrrightService.postRrrightOwnerProperty(params)
       .subscribe((res: any) => {
         this.close()
@@ -116,5 +133,15 @@ export class AddPropertyOwnerComponent implements OnInit {
       .subscribe((newCustomer) => {
         console.log(newCustomer)
       })
+  }
+
+  private createFractionValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const fraction = Number(control.value)
+      if (fraction + this.fractions_sum > 1 || fraction < 0) {
+        return { 'invalidFraction': true }
+      }
+      return null
+    }
   }
 }
