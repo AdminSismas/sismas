3c4363e86d56eff931f7e4bd48f5b19b17d17cbe
david.aristizabal <david.aristizabal@kiplan.io>
2024-11-20 08:56:18 -0500

    Corrección de bugs con respecto a la edición de propietarios

--- src/app/apps/components/information-property/information-property-owners/editing-property-owner/editing-property-owner.component.ts
+++ src/app/apps/components/information-property/information-property-owners/editing-property-owner/editing-property-owner.component.ts
@@ -57,6 +57,8 @@ export class EditingPropertyOwnerComponent implements OnInit {
   ngOnInit(): void {
     const formValues = this.data.rrrightInfo
 
+    console.log(this.data)
+
     this.form.reset(formValues)
   }
 
@@ -66,7 +68,6 @@ export class EditingPropertyOwnerComponent implements OnInit {
 
   editRrrightOwnerProperty(): any {
     const values = this.form.value
-    values.fraction = values.fraction / 100
 
     try {
       values.beginAt = values.beginAt.toISOString().split('T')[0]
@@ -74,11 +75,13 @@ export class EditingPropertyOwnerComponent implements OnInit {
       values.beginAt = values.beginAt
     }
 
+
+
     values.rightId = this.data.rightId
 
-    const { number, domIndividualEthnicGroup } = this.data.individual
+    const { number, domIndividualTypeNumber } = this.data.individual
 
-    this.peopleService.getPeopleTypeNumber({ number: number as string, individualTypeNumber: domIndividualEthnicGroup as string })
+    this.peopleService.getPeopleTypeNumber({ number: number, individualTypeNumber: domIndividualTypeNumber })
       .subscribe((res: InfoPerson) => {
         values.individual = { individualId: res.individualId }
         this.rrrightService.updatePropertyOwner({
