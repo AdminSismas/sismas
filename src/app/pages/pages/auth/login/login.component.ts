5a44763d1bda2cb63d74e16af0404d47fadd6d39
julian-gonzalez-agencycic <edgar.gonzalez@kubico.co>
2024-12-05 23:17:33 -0500

    inicio de sesión y recuperación del token

--- src/app/pages/pages/auth/login/login.component.ts
+++ src/app/pages/pages/auth/login/login.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { fadeInUp400ms } from '@vex/animations/fade-in-up.animation';
@@ -12,6 +12,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { environment } from '../../../../../environments/environments';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'vex-login',
@@ -36,11 +37,7 @@ import { environment } from '../../../../../environments/environments';
   ]
 })
 export class LoginComponent {
-  form = this.fb.group({
-    email: ['', Validators.required],
-    password: ['', Validators.required]
-  });
-
+  form!: FormGroup;
   inputType = 'password';
   visible = false;
 
@@ -48,21 +45,47 @@ export class LoginComponent {
     private router: Router,
     private fb: FormBuilder,
     private cd: ChangeDetectorRef,
-    private snackbar: MatSnackBar
-  ) {}
-
-  send() {
-    this.router.navigate([`${environment.myWork_cadastralSearch}`])
-      .then(value => {});
-    this.snackbar.open(
-      "Bienvenido usuario ;)",
-      'Gracias',
-      {
-        duration: 10000
-      }
-    );
+    private snackbar: MatSnackBar,
+    private authService: AuthService
+  ) {
+    this.form = this.fb.group({
+      email: ['', [Validators.required]],
+      password: ['', Validators.required]
+    });
   }
 
+  send() {
+    if (this.form.valid) {
+      const { email, password } = this.form.value;
+      
+      this.authService.login(email, password).subscribe(
+        (response: any) => {
+          if (response && response.token) {
+            this.authService.saveToken(response.token);
+            this.router.navigate([`${environment.myWork_cadastralSearch}`])
+              .then(() => {
+                this.snackbar.open('Bienvenido usuario ;)', 'Gracias', {
+                  duration: 5000
+                });
+              });
+          } else {
+            this.snackbar.open('Credenciales incorrectas. Intenta nuevamente.', 'Error', {
+              duration: 5000
+            });
+          }
+        },
+        (error) => {
+          this.snackbar.open('Credenciales incorrectas. Intenta nuevamente.', 'Error', {
+            duration: 5000
+          });
+        }
+      );
+    } else {
+      this.snackbar.open('Por favor, complete los campos correctamente.', 'Error', {
+        duration: 3000
+      });
+    }
+  }
   toggleVisibility() {
     if (this.visible) {
       this.inputType = 'password';
--- src/app/pages/pages/auth/login/services/auth.service.ts
+++ src/app/pages/pages/auth/login/services/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../../../../../environments/environments';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 
 @Injectable({
@@ -12,32 +14,74 @@ export class AuthService {
 
   private urlEndpoint = `${environment.url}:${environment.port}/auth/login`;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
+
+
 
   public get token(): string | null {
-    if (this._token != null) {
+    if (this._token) {
       return this._token;
     }
-    if (this._token == null && (sessionStorage == null || sessionStorage.getItem('token') == null)) {
-      return null;
+   
+    if (sessionStorage.getItem('token')) {
+      this._token = sessionStorage.getItem('token');
+      return this._token;
     }
-
-    this._token = sessionStorage.getItem('token');
-    return this._token;
+    return null;
   }
 
   saveToken(access_token: string) {
     this._token = access_token;
     try {
-      sessionStorage.setItem("token",this._token);
+      sessionStorage.setItem('token', this._token);
     } catch (error) {
+      console.error('Error al guardar el token', error);
     }
   }
+
+  // decodeToken(token: string) {
+  //   const decoded: any = jwt_decode(token);
+  //   console.log(decoded); 
+  //   return decoded;
+  // }
+
+  // decodeToken(token: string, secret: string) {
+  //   try {
+     
+  //     const decoded = jwt.verify(token, secret);
+  //     console.log(decoded); 
+  //     return decoded;
+  //   } catch (error) {
+  //     console.error('Token inválido o expirado');
+  //     return null;
+  //   }
+  // }
 
   refreshToken() {
     const httpOptions = {
       headers: new HttpHeaders({ 'Content-Type': 'application/json' })
     };
     return this.http.post<any>(this.urlEndpoint,httpOptions);
+  }
+
+
+  login(email: string, password: string): Observable<any> {
+    const body = { username: email, password: password };
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    };
+
+    return this.http.post<any>(this.urlEndpoint, body, httpOptions);
+  }
+
+  isAuthenticated(): boolean {
+    return this.token !== null;
+  }
+
+
+  logout(): void {
+    this._token = null;
+    sessionStorage.removeItem('token');
+    this.router.navigate(['/login']);
   }
 }
