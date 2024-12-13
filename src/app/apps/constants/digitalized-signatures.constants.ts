a7e029c3ee15b803dad66b8bd985a130c33178e9
david.aristizabal <david.aristizabal@kiplan.io>
2024-12-13 13:54:59 -0500

    Creado el archivo de constantes para la página de firmas digitalizadas

---
+++ src/app/apps/constants/digitalized-signatures.constants.ts
@@ -0,0 +1,13 @@
+const DIGITALIZED_SIGNATURES_DATA = {
+  "name": "Ajuste de propietario",
+  "description": "Permite ajustar el nombre del individuo de los derechos del predio",
+  "bpmProcessCategory": "M_PRIMERA",
+  "key": "P_M1_UIDP",
+  "version": "1.0",
+  "resource": "",
+  "image": "",
+  "validBeginAt": "2023-09-24",
+  "validToAt": "2023-09-24",
+  "dueDays": 5,
+  "rev": ""
+}
