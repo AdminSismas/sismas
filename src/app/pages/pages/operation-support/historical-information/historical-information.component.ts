608d1e203f248d35400a94124076f07472b1ff8c
Cristian-Munoz-Agencycic <cristian.munoz@agencycic.com>
2024-12-13 12:36:28 -0500

    Informacion historica #100, Inicio implementacion pantalla

--- src/app/pages/pages/operation-support/historical-information/historical-information.component.ts
+++ src/app/pages/pages/operation-support/historical-information/historical-information.component.ts
@@ -3,15 +3,54 @@ import { InConstructionComponent } from '../../../../apps/components/in-construc
 import { MatIconModule } from '@angular/material/icon';
 import { VexBreadcrumbsComponent } from '@vex/components/vex-breadcrumbs/vex-breadcrumbs.component';
 import { VexSecondaryToolbarComponent } from '@vex/components/vex-secondary-toolbar/vex-secondary-toolbar.component';
+import { VexPageLayoutComponent } from '@vex/components/vex-page-layout/vex-page-layout.component';
+import { VexPageLayoutHeaderDirective } from '@vex/components/vex-page-layout/vex-page-layout-header.directive';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { VexPageLayoutContentDirective } from '@vex/components/vex-page-layout/vex-page-layout-content.directive';
+import { AsyncPipe, NgClass, NgFor, NgIf } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { MatPseudoCheckboxModule } from '@angular/material/core';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatSelectModule } from '@angular/material/select';
+import { FluidHeightDirective } from 'src/app/apps/directives/fluid-height.directive';
 
 @Component({
   selector: 'vex-historical-information',
   standalone: true,
   imports: [
-    InConstructionComponent,
-    MatIconModule,
+    VexPageLayoutComponent,
+    VexPageLayoutHeaderDirective ,
     VexBreadcrumbsComponent,
-    VexSecondaryToolbarComponent
+    MatButtonToggleModule,
+    ReactiveFormsModule,
+    VexPageLayoutContentDirective,
+    NgIf,
+    MatButtonModule,
+    MatTooltipModule,
+    MatIconModule,
+    MatMenuModule,
+    MatTableModule,
+    MatSortModule,
+    MatPseudoCheckboxModule,
+    NgFor,
+    NgClass,
+    MatPaginatorModule,
+    FormsModule,
+    MatDialogModule,
+    MatInputModule,
+    MatTabsModule,
+    MatSelectModule,
+    AsyncPipe,
+    VexSecondaryToolbarComponent,
+    FluidHeightDirective
   ],
   templateUrl: './historical-information.component.html',
   styleUrl: './historical-information.component.scss'
