2d6bed600d539234b4b940a37b3f0c90181a3d1d
Cristian-Munoz-Agencycic <cristian.munoz@agencycic.com>
2024-12-16 12:16:43 -0500

    Formateo de campos entre tap de busqueda

--- src/app/pages/pages/operation-support/historical-information/filter-historical-information/filter-historical-information.component.ts
+++ src/app/pages/pages/operation-support/historical-information/filter-historical-information/filter-historical-information.component.ts
@@ -247,17 +247,20 @@ export class FilterHistoricalInformationComponent {
     public clearFormFields(value:any){
       console.log('value',value?.tab?.textLabel )
   
-      if(value?.tab?.textLabel === 'Seleccion Municipal'){
+      if(value?.tab?.textLabel === 'Selección Municipal'){
         this.clearMultipleFields()
+        this.formatFieldValue();    
       }
   
-      if(value?.tab?.textLabel === 'Multiplex Campos'){
-        this.clearMunicipalSelection()
-      }
-  
-      if(value?.tab?.textLabel === 'Numero Predial Nacional'){
+      if(value?.tab?.textLabel === 'Múltiples campos'){
         this.clearMunicipalSelection();
         this.formatFieldValue();
+      }
+  
+      if(value?.tab?.textLabel === 'Número Predial Nacional'){
+        this.clearMunicipalSelection();
+        this.clearMultipleFields()
+        // this.form.reset()  
   
       }
     }
