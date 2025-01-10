340a5bb2fb1a04ab4a4fe116d6a818b4c223c11b
Jhordan Steve Caro Hernandez <jhordan.caro@kubico.co>
2025-01-10 12:14:34 -0500

    carrusel quemado en vista de registro de acceso

--- src/app/pages/pages/audit/access-records/access-records.component.ts
+++ src/app/pages/pages/audit/access-records/access-records.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { InConstructionComponent } from '../../../../apps/components/in-construction/in-construction.component';
 import { MatIconModule } from '@angular/material/icon';
 import { VexBreadcrumbsComponent } from '@vex/components/vex-breadcrumbs/vex-breadcrumbs.component';
@@ -8,14 +9,39 @@ import { VexSecondaryToolbarComponent } from '@vex/components/vex-secondary-tool
   selector: 'vex-access-records',
   standalone: true,
   imports: [
+    CommonModule,
     InConstructionComponent,
     MatIconModule,
     VexBreadcrumbsComponent,
     VexSecondaryToolbarComponent
   ],
   templateUrl: './access-records.component.html',
-  styleUrl: './access-records.component.scss'
+  styleUrls: ['./access-records.component.scss']
 })
-export class AccessRecordsComponent {
+export class AccessRecordsComponent implements OnInit {
 
+  currentIndex = 0;
+  images: string[] = [
+    'assets/img/slider/slider1.jpg',
+    'assets/img/slider/slider2.jpg',
+    'assets/img/slider/slider3.jpg'
+  ];
+
+  ngOnInit(): void {
+    this.autoSlide();
+  }
+
+  nextSlide(): void {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
+
+  prevSlide(): void {
+    this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+  }
+
+  autoSlide(): void {
+    setInterval(() => {
+      this.nextSlide();
+    }, 5000);
+  }
 }
