b088667dab970246ead11a60b9e8b4be9cd80f61
draristizabal <david.aristizabal@agencycic.com>
2025-01-10 15:12:32 -0500

    antigua y nueva contraseña diferentes y nueva contraseña y confirmar contraseña iguales funciona

--- src/app/apps/components/sidenav/menu/change-password/change-password.component.ts
+++ src/app/apps/components/sidenav/menu/change-password/change-password.component.ts
@@ -1,4 +1,5 @@
 // Angular framework
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 // Vex
@@ -10,13 +11,20 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { GeneralValidationsService } from 'src/app/apps/services/validations/general-validations.service';
 // Custom
 
+interface FormErrors {
+  required: boolean;
+  samePassword: boolean;
+  passwordMismatch: boolean;
+}
 
 @Component({
   selector: 'vex-change-password',
   standalone: true,
   imports: [
+    CommonModule,
     ReactiveFormsModule,
     // Vex
     // Material
@@ -33,20 +41,42 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class ChangePasswordComponent {
   public form: FormGroup = this.fb.group({
-    oldPassword: ['', Validators.required],
+    lastPassword: ['', Validators.required],
     newPassword: ['', Validators.required],
     confirmPassword: ['', Validators.required]
+  }, {
+    validators: [
+      this.generalValidations.isFieldOneDifferentToFieldTwo('lastPassword', 'newPassword'),
+      this.generalValidations.isFieldOneEqualsToFieldTwo('newPassword', 'confirmPassword'),
+    ]
   });
+  public lastPasswordError = '';
+  public newPasswordError = '';
+  public confirmPasswordError = '';
 
   constructor(
     private fb: FormBuilder,
     private snackbar: MatSnackBar,
-    private dialogRef: MatDialogRef<ChangePasswordComponent>
-  ) {}
+    private dialogRef: MatDialogRef<ChangePasswordComponent>,
+    private generalValidations: GeneralValidationsService
+  ) { }
 
   changePassword(): void {
+    this.form.markAllAsTouched();
+    if (this.form.errors) {
+      if (this.form.errors['samePassword']) {
+        this.snackbar.open('La nueva contraseña no puede ser la misma que la antigua', 'CLOSE', {
+          duration: 3000
+        });
+        return;
+      } else if (this.form.errors['passwordMismatch']) {
+        this.snackbar.open('Las nuevas contraseñas no coinciden', 'CLOSE', {
+          duration: 3000
+        });
+        return;
+      }
+    }
     if (this.form.invalid) {
-      this.form.markAllAsTouched();
       this.snackbar.open('Por favor, completa todos los campos', 'CLOSE', {
         duration: 3000
       });
@@ -58,7 +88,45 @@ export class ChangePasswordComponent {
   }
 
   invalidInput(control: string): boolean {
-    const { invalid, touched } = this.form.controls[control];
-    return invalid && touched;
+    const { invalid, touched, errors } = this.form.controls[control];
+    let response: boolean = false;
+
+    if (!invalid || !touched) {
+      return response;
+    }
+
+    switch (control) {
+      case 'lastPassword':
+        if (invalid && touched) {
+          this.lastPasswordError = errors?.['required']
+            ? 'Por favor, introduce tu antigua contraseña'
+            : '';
+        }
+        response = true;
+        break;
+      case 'newPassword':
+        console.log('Validando el nuevo password');
+        if (invalid && touched ) {
+          this.newPasswordError = errors?.['required']
+            ? 'Por favor, introduce tu nueva contraseña'
+            : errors?.['isEquals']
+            ? 'La nueva contraseña no puede ser la misma que la antigua'
+            : '';
+        }
+        response = true;
+        break;
+      case 'confirmPassword':
+        if (invalid && touched ) {
+          this.confirmPasswordError = errors?.['required']
+            ? 'Por favor, confirmar la nueva contraseña'
+            : errors?.['notEquals']
+            ? 'Las nuevas contraseñas no coinciden'
+            : '';
+        }
+        response = true;
+        break;
+    }
+
+    return response;
   }
 }
