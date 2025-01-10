f7fbc07d0b63148bd1e267221c5f1047dca0b548
Cristian-Munoz-Agencycic <cristian.munoz@agencycic.com>
2025-01-10 09:34:03 -0500

    Editar el Html desde el front #83, Se implementa la tabla listar las plantillas que se van a modificar

---
+++ src/app/apps/services/document-associated.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environments';
+import { DocumentAsocietyModel } from '../interfaces/document-asociety.model';
+import { Observable } from 'rxjs';
+import { InformationPegeable } from '../interfaces/information-pegeable.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DocumentAssociatedService {
+
+  basic_url: string = `${environment.url}:${environment.port}`;
+
+  constructor(private http: HttpClient) { }
+
+
+  /* -------------- MÉTODOS -------------- */
+getDataDocumentoAsociety(page: any):Observable<InformationPegeable> {
+  let paramsPP:HttpParams = new HttpParams();
+  paramsPP = paramsPP.append('page', `${page.page}`);
+  paramsPP = paramsPP.append('size', `${page.size}`);
+  const urlComplete = `${this.basic_url}/bpmOutTemplate?page=${page.page}&size=${page.size}&sortBy=templateCode`;
+
+  // console.log('link: ',`${this.basic_url}?` + paramsPP.toString());
+  return  this.http.get<InformationPegeable>(urlComplete);
+}
+
+}
