65254acf01417730d16bf516f928a077e5e548db
draristizabal <david.aristizabal@agencycic.com>
2025-01-14 00:33:43 -0500

    Componente de propietarios funcionando

--- src/app/apps/components/information-property/information-property-owners/information-property-owners.component.ts
+++ src/app/apps/components/information-property/information-property-owners/information-property-owners.component.ts
@@ -32,7 +32,6 @@ import { AddPropertyOwnerComponent } from './add-property-owner/add-property-own
 import { DeletePropertyOwnerComponent } from './delete-property-owner/delete-property-owner.component';
 import { EditingPropertyOwnerComponent } from './editing-property-owner/editing-property-owner.component';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { Owners } from 'src/app/apps/interfaces/bpm/changes-property-owner';
 import { CommonModule } from '@angular/common';
 
 export type InfoOwnerRowT = Pick<InfoOwners, 'rightId' | 'beginAt' | 'fractionS' | 'domRightType'> &
@@ -50,6 +49,7 @@ export type InfoOwnerRowT = Pick<InfoOwners, 'rightId' | 'beginAt' | 'fractionS'
     scaleFadeIn400ms
   ],
   imports: [
+    CommonModule,
     HeaderCadastralInformationPropertyComponent,
     MatCardModule,
     MatRippleModule,
@@ -60,7 +60,6 @@ export type InfoOwnerRowT = Pick<InfoOwners, 'rightId' | 'beginAt' | 'fractionS'
     MatMenuModule,
     MatPaginatorModule,
     MatDialogModule,
-    CommonModule
   ],
   templateUrl: './information-property-owners.component.html',
   styleUrl: './information-property-owners.component.scss'
@@ -187,7 +186,7 @@ export class InformationPropertyOwnersComponent implements OnInit, AfterViewInit
     this.id = this.id + this.getRandomInt(10000) + this.schema;
     this.isExpandPanel(this.expandedComponent);
 
-    this.TABLE_COLUMNS.at(-1)!.visible = this.typeInformation === 'edition';
+    this.TABLE_COLUMNS.at(-1)!.visible = this.typeInformation === 'edition' && this.editable;
   }
 
   isExpandPanel(expandedComponent: boolean): void {
