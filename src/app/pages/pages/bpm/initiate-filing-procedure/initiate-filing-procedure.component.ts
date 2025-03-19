a7bfd81bf672fae5ef34cdf3d660287178e1c54a
draristizabal <david.aristizabal@agencycic.com>
2025-03-19 16:48:23 -0500

    Corrección error al seleccionar clase al radicar un nuevo proceso

--- src/app/pages/pages/bpm/initiate-filing-procedure/initiate-filing-procedure.component.ts
+++ src/app/pages/pages/bpm/initiate-filing-procedure/initiate-filing-procedure.component.ts
@@ -157,6 +157,7 @@ export class InitiateFilingProcedureComponent implements OnInit {
   listBpmDocumentsProcess$: Observable<BpmDocument[]> =
     this._listBpmDocumentsProcess.asObservable();
   isExistListBpmTypeProcess$: Observable<boolean> = of(false);
+  bpmProccessCategorySelected = false;
 
   baunitIdE: string | null = null;
   baunitIdE$ = this.route.params.pipe(
@@ -270,6 +271,7 @@ export class InitiateFilingProcedureComponent implements OnInit {
   }
 
   loadBpmProcess(domainCollection: DomainCollection) {
+    this.bpmProccessCategorySelected = true;
     if (!domainCollection || !domainCollection?.code) {
       return;
     }
