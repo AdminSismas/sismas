89983a386fe20f62105e56ff5c25392efeab6ae3
VictorGalvis <user>
2025-04-11 13:57:03 +0200

    Ajustes en manejo de objetos de unidades matriz y unidades prediales de matriz, edicion y visualizacion de los datos, en general

---
+++ src/app/apps/interfaces/information-property/basic-detail-group.ts
@@ -0,0 +1,16 @@
+export class BasicDetailGroup {
+
+  buildNumber?: number;
+  floorNumber?: number;
+  percentageGroupS?: string;
+  percentage_group?: number;
+  unitNumber?: number;
+
+  constructor(content?: any) {
+    this.buildNumber = content?.buildNumber || 0;
+    this.floorNumber = content?.floorNumber || 0;
+    this.percentageGroupS = content?.percentageGroupS || '';
+    this.percentage_group = content?.percentage_group || 0;
+    this.unitNumber = content?.unitNumber || 0;
+  }
+}
