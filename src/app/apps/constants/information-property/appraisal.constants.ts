8dd6765a5f2355fd4be94152493a3b5d548ec809
draristizabal <david.aristizabal@agencycic.com>
2025-06-05 14:06:52 -0500

    710 corrección autoavalúo (#712)
    * Asignar zonas predios matriz y unidades prediales (#707)
    
    * Eliminación de impresiones de consola
    
    * la variable isMatriz ya está funcionando en el componente ingotmation-zones-property.component
    
    * variable isMatriz ahora es recibida por las tablas de las zonas
    
    * Botón "Asignar" funcional en las tablas de zonas Físicas Alfanuméricas
    
    * Endpoint enlasado y operando (Aún no se encuentra dado que necesita una actualización del backend)
    
    * Endpoint agregado al botón de "Asignar zonas" en la ventana modal desplegada cuando se da click en "Editar unidades"
    
    * Corrección a las urls de los endpoints de los servicios. Tanto para los servicios de la pestaña "Zonas" como la ventana modal "Editar unidades"
    
    ---------
    
    Co-authored-by: draristizabalr <draristizabalr@unal.edu.co>
    
    * Mensaje de confirmación en los procesos de asignación de zonas
    
    * Adminstración de construcciones (#709)
    
    * Corrección de errores y mejoras con el componente information-constructins-property.component
    
    * Creación de interface para el manejo de los tamaños de las modales
    
    * Creado el botón "Administrar" con la ventana modal que se despliega al dar click, en la pestaña de "Construcciones"
    
    * Se tiene el endpoint de obtención de las construcciones de un predio borrado en la ventana modal que se abre al presionar el botón "Adminsitrar"
    
    * Separada la tabla de las construcciones del componente information-construcionns-property.components para poder ser reutilizado
    
    * La ventana modal ya muestra la tabla de contrucciones que no tienen baunit asiganadas
    
    * Selección de construcción funcionando
    
    * Servicio de adminstración de construcción funcionando correctamente
    
    * Correcciones de estilos y mensaje de confirmación, más actulización automática de información, cuando se realiza el proceso correctamente
    
    ---------
    
    Co-authored-by: draristizabalr <draristizabalr@unal.edu.co>
    
    * Funciones de menú desplegable de las construcciones reparado
    
    * Cambios de formato de directivas a workflow sintaxis de las nuevas versiones de Angular al componente property-appraisal-information.component
    
    * Se está usando el componente de dynamic-forms.component en el componente auto-appraisal.component
    
    * Configuración del formulario completada
    
    * Validación y servicio modificado para el consumo del endpoint
    
    ---------
    
    Co-authored-by: draristizabalr <draristizabalr@unal.edu.co>

---
+++ src/app/apps/constants/information-property/appraisal.constants.ts
@@ -0,0 +1,49 @@
+import { Validators } from "@angular/forms";
+import { JSONInput } from "../../interfaces/forms/dynamic-forms";
+
+export const AUTO_APPRAILSAL_INPUTS: JSONInput[] = [
+  {
+    name: "validity",
+    label: "Vigencia",
+    placeholder: "Seleccionar vigencia",
+    element: "select",
+    type: "",
+    validators: [Validators.required],
+    options: [],
+    cssClasses: "w-full"
+  },
+  {
+    name: "toggleValuationType",
+    label: "Autoavalúo total",
+    placeholder: "",
+    element: "toggle",
+    type: "",
+    validators: [],
+    cssClasses: "w-full flex flex-col items-center justify-center"
+  },
+  {
+    name: "selfValuationValueLand",
+    label: "Autoavalúo terreno",
+    placeholder: "Escribir autoavalúo terreno",
+    element: "input",
+    type: "text",
+    validators: []
+  },
+  {
+    name: "selfValuationValueUnits",
+    label: "Autoavalúo construcciones",
+    placeholder: "Escribir autoavalúo construcciones",
+    element: "input",
+    type: "text",
+    validators: []
+  },
+  {
+    name: "selfValuationValue",
+    label: "Autoavalúo total",
+    placeholder: "Escribir autoavalúo total",
+    element: "input",
+    type: "text",
+    validators: [],
+    cssClasses: "w-full col-span-2"
+  }
+];
