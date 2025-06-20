efba67c56f1e6f0d93403b6a34538e36b4192590
draristizabal <david.aristizabal@agencycic.com>
2025-06-20 15:33:02 -0500

    Cambios de estilos a la ventana modal show-error-validate-alfa-main.component (Errores de validación). Ahora es posible seleccionar el texto para copiarlo y además al hacer click se copia el texto seleccionado al portapapeles del usuario

--- src/app/apps/components/bpm/show-error-validate-alfa-main/show-error-validate-alfa-main.component.ts
+++ src/app/apps/components/bpm/show-error-validate-alfa-main/show-error-validate-alfa-main.component.ts
@@ -2,47 +2,42 @@ import { Component, DestroyRef, inject, Inject, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatRippleModule } from '@angular/material/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { MatStepperModule } from '@angular/material/stepper';
-import { FluidHeightDirective } from '../../../directives/fluid-height.directive';
-import { FormsModule, ReactiveFormsModule, UntypedFormControl } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
+import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+import {
+  ReactiveFormsModule,
+  UntypedFormControl
+} from '@angular/forms';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { NgIf } from '@angular/common';
+import { MatDividerModule } from '@angular/material/divider';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'vex-show-error-validate-alfa-main',
   standalone: true,
   imports: [
     MatButtonModule,
+    MatDialogModule,
+    MatDividerModule,
     MatIconModule,
     MatRippleModule,
-    FluidHeightDirective,
-    MatStepperModule,
-    FluidHeightDirective,
-    FormsModule,
-    MatInputModule,
-    ReactiveFormsModule,
-    NgIf
+    ReactiveFormsModule
   ],
   templateUrl: './show-error-validate-alfa-main.component.html',
-  styleUrl: './show-error-validate-alfa-main.component.scss'
+  styleUrl: './show-error-validate-alfa-main.component.scss',
+  host: {
+    class: 'h-full overflow-hidden'
+  }
 })
 export class ShowErrorValidateAlfaMainComponent implements OnInit {
-
   listErrors: string[] = [];
   searchCtrl: UntypedFormControl = new UntypedFormControl();
 
   private readonly destroyRef: DestroyRef = inject(DestroyRef);
 
-  constructor(
-    @Inject(MAT_DIALOG_DATA) public defaults: string[],
-    private dialogRef: MatDialogRef<ShowErrorValidateAlfaMainComponent>,
-  ) {
-  }
+  constructor(@Inject(MAT_DIALOG_DATA) public defaults: string[]) {}
 
   ngOnInit() {
-    if(this.defaults && this.defaults.length > 0) {
+    if (this.defaults && this.defaults.length > 0) {
       this.listErrors = [...this.defaults];
     }
 
@@ -59,14 +54,22 @@ export class ShowErrorValidateAlfaMainComponent implements OnInit {
 
     value = value.trim();
     value = value.toLowerCase();
-    this.listErrors = this.defaults
-      .filter((val: string) => {
-        if (val) {
-          return val.toLowerCase().includes(value);
-        }
-        return val;
-      });
+    this.listErrors = this.defaults.filter((val: string) => {
+      if (val) {
+        return val.toLowerCase().includes(value);
+      }
+      return val;
+    });
+  }
+
+  copyError(error: string) {
+    navigator.clipboard.writeText(error);
+    Swal.fire({
+      text: 'Error copiado al portapapeles',
+      icon: 'info',
+      confirmButtonText: 'Aceptar',
+      confirmButtonColor: '#3085d6',
+      timer: 2000
+    });
   }
 }
-
-
