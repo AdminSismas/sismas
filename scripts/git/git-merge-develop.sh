3462d6495ee22c5a39bbe4800b36ad0140f6ed06
draristizabal <david.agencycic.com>
2025-07-01 13:43:34 -0500

    Creación de comando de git para mejorar el manejo del repositorio

---
+++ scripts/git/git-merge-develop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Script para hacer merge de despliegue-desarrollo a main
+git checkout despliegue-desarrollo && \
+git fetch origin -p && \
+git pull && \
+git checkout main && \
+git fetch origin -p && \
+git pull && \
+git merge despliegue-desarrollo -X theirs --no-edit && \
+git commit --amend -m "Merge despliegue-desarrollo into main - $(date +%d-%m-%Y)" && \
+echo -n "¿Desea realizar un push al main? (s/n): " && \
+read response && \
+response=$(echo "$response" | tr '[:upper:]' '[:lower:]') && \
+case "$response" in
+  s) echo "Realizando push al main..." && git push;;
+  n) echo "Operación cancelada." ;;
+  *) echo "Respuesta no válida. Operación cancelada." ;;
+esac
