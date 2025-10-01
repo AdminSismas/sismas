283d43b74de066d0d95af52de6aedeef74705da6
draristizabal <david.aristizabal@agencycic.com>
2025-09-30 19:39:34 -0500

    Creación de script, de bash, para el despliegue a los cinco (5) ambientes de producción

---
+++ scripts/git/deploy-all.sh
@@ -0,0 +1,5 @@
+gh workflow run "Deploy to Manizales" &&\
+gh workflow run "Deploy to Masora" &&\
+gh workflow run "Deploy to Montenegro" &&\
+gh workflow run "Deploy to Quimbaya" &&\
+gh workflow run "Deploy to Filandia"
